e13afe2dfe236a479ed1f39a6d3686910be5d926
Elijah Jacob <elijahjacob3402@gmail.com>
2024-11-22 16:46:03 +0100

    display the questions

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ let questions = [];
 
 //get questions
 
-fetch('./questions.json')
+fetch('.questions.json')
     .then(response => response.json())
     .then(data => {
         questions = data;
